766296ee56d277759a2a7a3b588301ed2b5a83d1
Swizzle <miringanita@gmail.com>
2020-04-20 23:23:29 +0200

    adding basic ingredeint funcionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-// import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from '@angular/common/http';
 
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { IngredientComponent } from './ingredient/ingredient.component';
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./header/header.component";
+import { IngredientComponent } from "./ingredient/ingredient.component";
+import { RecipeComponent } from "./recipe/recipe.component";
+import { RecipeCreateComponent } from "./recipe/recipe-create/recipe-create.component";
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     IngredientComponent,
-    // HttpClientModule
+    RecipeComponent,
+    RecipeCreateComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
