2d2ad11dec4e45041f7e00ed8b0f40d2ce517a17
Swizzle <miringanita@gmail.com>
2020-04-21 21:26:31 +0200

    addind recipes list component

---
+++ src/app/recipe/recipe-list/recipe-list.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import {Recipe} from "../recipe.model";
+import {RecipeService} from "../recipe.service"
+
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit, OnDestroy{
+
+  recipes: Recipe[] = [];
+  private recipeSub: Subscription;
+
+  constructor(public recipeService: RecipeService) {}
+
+  ngOnInit() {
+    this.recipeService.getRecipes();
+    this.recipeSub = this.recipeService.getRecipeUpdateListener()
+      .subscribe((recipes: Recipe[]) => {
+        this.recipes = recipes;
+      });
+  }
+
+  ngOnDestroy() {
+    this.recipeSub.unsubscribe();
+  }
+
+}
